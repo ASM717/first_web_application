6e8d9bd031990ac745158784331130c6dd6587f8
Allyson Muriel <amuriel@mi-a6.msk.21-school.ru>
2022-05-16 17:48:16 +0300

    html pages add to project

--- ex00/Cinema/src/main/java/edu/school21/cinema/Program.java
+++
@@ -1,7 +0,0 @@
-package edu.school21.cinema;
-
-import javax.servlet.http.HttpServlet;
-
-public class Program extends HttpServlet {
-
-}
---
+++ ex00/Cinema/src/main/java/edu/school21/cinema/models/User.java
@@ -0,0 +1,48 @@
+package edu.school21.cinema.models;
+
+public class User {
+//    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String password;
+
+    public User(String firstName, String lastName, String phoneNumber, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
