b531176ca2c38ba95922bb3be82f9ab66a010222
Stepan A <56968976+ASM717@users.noreply.github.com>
2022-05-19 22:10:56 +0300

    del target from repo

--- ex00/Cinema/target/classes/sql/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO users.users (first_name, last_name, phone_number, password)
-VALUES ('Elena', 'mstyr', '+79991111111', '$3a@12$e'),
-       ('Gregory', 'esobchak', '+79992222222', '$3a@12$1O'),
-       ('Ksenia', 'maragorn', '+79993333333', '$3a@12$'),
-       ('Vladimir', 'eshakita', '+79994444444', '$3a@12$nH9'),
-       ('Daria', 'cchromos', '+79995555555', '$3a@12$Re'),
-       ('Marina', 'mcerope', '+79996666666', '$3a@787$Re')
-    ON CONFLICT DO NOTHING;
