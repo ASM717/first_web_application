b06841a6a0f6f862ac115d000aa9103ebf28be18
ASM717 <stepan151@gmail.com>
2022-05-25 09:15:44 +0300

    getRequestDispatcher (signUp.jsp)

--- ex00/Cinema/src/main/java/edu/school21/cinema/servlets/SignUpServlet.java
+++ ex00/Cinema/src/main/java/edu/school21/cinema/servlets/SignUpServlet.java
@@ -12,13 +12,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebServlet("/signUp")
+@WebServlet("/signUp")
 public class SignUpServlet extends HttpServlet {
     private UsersService usersService;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/signUp").forward(request, response);
+        request.getRequestDispatcher("/WEB-INF/jsp/signUp.jsp").forward(request, response);
     }
 
     @Override
@@ -26,10 +26,10 @@ public class SignUpServlet extends HttpServlet {
 
     }
 
-    @Override
-    public void init (ServletConfig config ) throws ServletException {
-        ServletContext context = config.getServletContext();
-        ApplicationContext springContext = (ApplicationContext) context.getAttribute("springContext");
-        this.usersService = springContext.getBean(UsersService.class);
-    }
+//    @Override
+//    public void init (ServletConfig config ) throws ServletException {
+//        ServletContext context = config.getServletContext();
+//        ApplicationContext springContext = (ApplicationContext) context.getAttribute("springContext");
+//        this.usersService = springContext.getBean(UsersService.class);
+//    }
 }
